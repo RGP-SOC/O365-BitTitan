b1205ed321a54ebaf1b0bb6effc55e77698f51c4
Andrew Heberle <andrew.heberle@gmail.com>
2017-04-03 13:34:31 +0800

    Make UPN script more generic, add error checking in more places

--- 365_CSV.ps1
+++ 365_CSV.ps1
@@ -1,7 +1,20 @@
-#./Script-Name.ps1 -SourceTenant <Source> -TargetTenant <Target> -VanityDomain <Email Domain> |export-csv PATH
-
-
-[cmdletbinding()]
+<#
+.SYNOPSIS
+Exports user list from Office 365 tentant in a format that can be used to bulk-add users in BitTitan
+.PARAMETER SourceTenant
+The name of the source tentant.
+.PARAMETER TargetTenant
+The name of the target tentant.
+.PARAMETER VanityDomain
+An array of "vanity domains" in use by the organisation.
+.PARAMETER Flags
+Flags that affect the migration process in BitTitan. See the BitTitan KB for more info.
+.PARAMETER Credential
+The credentials to log onto the source tenant.
+.EXAMPLE
+./Script-Name.ps1 -Credential (Get-Credential) -SourceTenant sourcecompany.onmicrosoft.com -TargetTenant targettenant.onmicrosoft.com 
+#>
+[cmdletbinding(SupportsShouldProcess=$True)]
 Param (
     [Parameter(Mandatory=$true)]
     [String]$SourceTenant,
@@ -13,25 +26,33 @@ Param (
     [PsCredential]$Credential
 )
 BEGIN {
-    Connect-MsolService -Credential $Credential
+    $Session = New-PsSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -AllowRedirect -Authentication Basic -Credential $Credential
+    Import-PsSession -AllowClobber $Session | Out-Null
 }
 PROCESS {
     foreach ($d in $VanityDomain) {
         try {
-            foreach ($u in (Get-MsolUser -All | ? { $_.IsLicensed } | ? { $_.UserPrincipalName -like "*@$($d)" }) ) {
+            foreach ($u in (Get-Mailbox -ResultSize Unlimited | ? { $_.UserPrincipalName -like "*@$($d)" }) ) {
                 $out = [ordered]@{}
-                $out.'Source Email' = $u.UserPrincipalName.Replace($d, $SourceTenant)
+                $out.'Source Email' = $u.UserPrincipalName.Replace("@$($d)", "@$($SourceTenant)")
                 $out.'Source Login Name' = $null
                 $out.'Source Password' = $null
-                $out.'Destination Email' = $u.UserPrincipalName.Replace($d, $TargetTenant)
+                $out.'Destination Email' = $u.UserPrincipalName.Replace("@$($d)", "@$($TargetTenant)")
                 $out.'Destination Login Name' = $null
                 $out.'Destination Password' = $null
                 $out.'Flags' = $Flags
                 [PsCustomObject]$out
             }
         } catch {
-            Write-Error "Problem with process..."
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            Write-Error "Problem with: $($FailedItem); Error: $($ErrorMessage)"
         }
     }
 }
-END { }
+END {
+    # Remove PsSession if required
+    if ($Session) {
+        Remove-PsSession $Session
+    }
+}
--- 365_set_UPN.ps1
+++ 365_set_UPN.ps1
@@ -1,31 +1,44 @@
-#./Script-Name.ps1 -Tenant <Tenant Name> -VanityDomain <Customers Email Domain>
-
+<#
+.SYNOPSIS
+Changes UserPrincipalName (Login Name) from SourceDomain to TargetDomain in the Office 365 Tenant
+.PARAMETER SourceDomain
+The name of the source domain.
+.PARAMETER TargetDomain
+The name of the target domain.
+.PARAMETER Credential
+The credentials to log onto the tenant.
+.PARAMETER Exdlue
+List of accounts to exlcude from the change.
+.EXAMPLE
+./Script-Name.ps1 -Credential (Get-Credential) -SourceDomain example.com -TargetDomain example.onmicrosoft.com 
+#>
 [cmdletbinding()]
 Param (
     [Parameter(Mandatory=$true)]
-    [String]$Tenant,
+    [String[]]$SourceDomain,
     [Parameter(Mandatory=$true)]
-    [String[]]$VanityDomain,
-    [String]$Exclude = "admin@$($Tenant)",
+    [String]$TargetDomain,
+    [String]$Exclude = "admin@$($SourceDomain)",
     [PsCredential]$Credential
 )
 BEGIN {
-    Connect-MsolService -Credential $Credential
+    Connect-MsolService -Credential $Credential -EA Stop
 }
 PROCESS {
-    foreach ($d in $VanityDomain) {
+    foreach ($d in $SourceDomain) {
         try {
         	foreach ($user in (Get-MsolUser -All | ? { ($_.UserPrincipalName -like "*@$($d)") -and ($_.UserPrincipalName -ne $Exclude) })) {
-                Set-MsolUserPrincipalName -UserPrincipalName $user.UserPrincipalName -NewUserPrincipalName ($user.UserPrincipalName.Split("@")[0] + "@$($Tenant)")
-                $user.UserPrincipalName
+                $NewUserPrincipalName = $user.UserPrincipalName.Split("@")[0] + "@$($TargetDomain)"
+                Set-MsolUserPrincipalName -UserPrincipalName $user.UserPrincipalName -NewUserPrincipalName $NewUserPrincipalName
+                    SourceUserPrincipalName = $user.UserPrincipalName;
+                    TargetUserPrincipalName = $NewUserPrincipalName
+                }
             }
         } catch {
-            Write-Error "Problem with process..."
+            $ErrorMessage = $_.Exception.Message
+            $FailedItem = $_.Exception.ItemName
+            Write-Error "Problem with: $($FailedItem); Error: $($ErrorMessage)"
         }
     }
 }  
 END { }
-
-Run the above:
-
-
